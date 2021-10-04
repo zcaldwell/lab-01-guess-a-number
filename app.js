f8cdfdab5b17d90fe49bf6206f48ca744632e108
Zachary Caldwell <mintyst.john@gmail.com>
2021-10-04 15:41:44 -0700

    Forgot to commit along the way, have HTML done and some JAVA

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 // import functions and grab DOM elements
+const numberInput = document.getElementById('number-input');
+const buttonInput = document.getElementById('guess')
+const answerResult = document.getElementById('answer-text')
 
 // initialize global state
-
+let guessNumber = 0;
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+  //if guess === answer "You are correct"
+  //if guesscount === 4 "you are out of guesses"
+  //if guess < answer "You are under"
+  //if guess > answer "you are over"
+let answer = Math.floor(Math.random() * 21);
+
+
+buttonInput.addEventListener('click', () => {
+    guessNumber++;
+    Number('number-input');)
+    let answerOutput;
+    if(numberInput === answer){
+      answerOutput = 'correct!';
+    } else if (guessNumber >= 4) {
+      answerOutput = 'out of guesses!';
+    } else if (numberInput > answer) {
+      answerOutput = 'Over!';
+    } else if (numberInput < answer) {
+      answerOutput = 'Under!';
+    }
+    const resultText = `You are ${answerOutput}.`;
+    answerResult.textContent = resultText;
+    
+});
+
+    
+    
+
