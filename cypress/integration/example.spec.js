9bebbc73289d2130fca9f944038d97c07a2b6275
Zachary Caldwell <85139250+zcaldwell@users.noreply.github.com>
2021-10-04 13:37:54 -0700

    Initial commit

---
+++ cypress/integration/example.spec.js
@@ -0,0 +1,12 @@
+
+describe('Example Test', ()=>{
+    before(()=>{
+        cy.visit('/');
+    });
+
+    describe('HTML elements', ()=>{   
+        it('contains the alchemy logo', ()=>{
+            cy.get('.main-logo').should('be.visible');
+        });
+    });
+});
