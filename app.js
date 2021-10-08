1ae2da59214c67e38aa2cad8f6d16a65ffb4b795
zcaldwell <mintyst.john@gmail.com>
2021-10-07 18:07:07 -0700

    working on last button

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const numberInput = document.getElementById('number-input');
 const buttonInput = document.getElementById('guess');
 const answerResult = document.getElementById('answer-text');
 const guessNumberCounter = document.getElementById('guess-counter');
+const rightCounter = document.getElementById('rightguess');
+const wrongCounter = document.getElementById('wrongguess');
+const tryAgainButton = document.getElementById('tryagainbutton');
 
 // console.log(guessNumberCounter);
 // initialize global state
@@ -14,21 +17,39 @@ let guessCount = 4;
   //if guess > answer "you are over"
 let answer = Math.floor(Math.random() * 21);
 // console.log(answer);
+let wrongguess = 0;
+let rightguess = 0;
+
 
 buttonInput.addEventListener('click', () => {
     if (guessCount >= 1) guessCount--;
     let num = Number(numberInput.value);
     let answerOutput;
-    if (num === answer){
+    if (num === answer) {  
         answerOutput = 'correct!';
+        rightguess++;
     } else if (guessCount <= 0) {
         answerOutput = 'out of guesses!';
     } else if (num > answer) {
         answerOutput = 'Over!';
+        wrongguess++;
     } else if (num < answer) {
         answerOutput = 'Under!';
+        wrongguess++;
+    console.log(answer);   
     }
     const resultText = `You are ${answerOutput}.`;
     answerResult.textContent = resultText;
     guessNumberCounter.textContent = guessCount;
+    rightCounter.textContent = rightguess;
+    wrongCounter.textContent = wrongguess;
+
+    
+});
+
+tryAgainButton.addEventListener('click', () => {
+    guessCount = 4;
+    const newAnswer = answer;
+    guessNumberCounter.textContent = guessCount;
+    console.log(newAnswer);
 });
--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,18 @@
 
-// describe('Example Test', ()=>{
-//     before(()=>{
-//         cy.visit('/');
-//     });
+describe('Example Test', ()=>{
+    before(()=>{
+        cy.visit('/');
+    });
 
-//     describe('HTML elements', ()=>{   
-//         it('contains the alchemy logo', ()=>{
-//             cy.get('.main-logo').should('be.visible');
-//         });
-//     });
-// });
+    describe('HTML elements', ()=>{   
+        it('contains the alchemy logo', ()=>{
+            cy.get('#number-input').should('be.visible');
+        });
+    });
+
+    describe('user interactions', () => {
+        it('should decrease the number of guesses');
+             cy.get('#guess');
+             cy.get('#answer-text').should('be.visible');
+    });
+});
